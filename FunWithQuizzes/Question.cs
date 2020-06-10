04afa51177c4f0242f5162e1e5266516e34afc64
lawrence <laguilera03@gmail.com>
2020-06-10 15:13:40 -0400

    Basic Coding and more design implemented

--- FunWithQuizzes/Question.cs
+++ FunWithQuizzes/Question.cs
@@ -18,6 +18,14 @@ namespace FunWithQuizzes
         Any inheritance relationship
         */
 
-
+        //ABCD multiple choice, 1234 checkbox, T or F for true or false
+        //Multiple choice takes in string, int, char
+        public string TheQuestion { get; set; }//Can't use Question as a name
+        public string Answer { get; set; }
+        public Question(string theQuestion, string answer)
+        {
+            TheQuestion = theQuestion;
+            Answer = answer;
+        }
     }
 }
--- FunWithQuizzes/Quiz.cs
+++ FunWithQuizzes/Quiz.cs
@@ -11,13 +11,18 @@ namespace FunWithQuizzes
          * Run or carry out the quiz
          * Grade the quiz
          */
+        public Question Question { get; set; }
 
         //add questions list from the question class
+        public Quiz(Question question) 
+        {
+            Question = question;
+        }
 
         //return the questions graded and answered
         public override string ToString()
         {
-            return null;
+            return "Grade: " + null;//return calculated grade
         }
     }
 }
