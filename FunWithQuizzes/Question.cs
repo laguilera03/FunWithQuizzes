86484ebd6d275d56a6ab656a98dbfc413b7e9003
lawrence <laguilera03@gmail.com>
2020-06-10 13:54:40 -0400

    Most Design complete 7.9.1

---
+++ FunWithQuizzes/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunWithQuizzes
+{
+    class Question
+    {
+        //Create get set for values
+
+        //Create a constructor for question / final answer
+        //Create a subclass for Multiple choice, checkbox, true/false
+        //sends question to the quiz class as a parameter in list format
+
+        /* Class name
+        Fields and properties with access modifiers
+        Methods with access modifiers
+        Any inheritance relationship
+        */
+
+
+    }
+}
---
+++ FunWithQuizzes/Quiz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunWithQuizzes
+{
+    class Quiz
+    {
+        /*
+         * Add questions
+         * Run or carry out the quiz
+         * Grade the quiz
+         */
+
+        //add questions list from the question class
+
+        //return the questions graded and answered
+        public override string ToString()
+        {
+            return null;
+        }
+    }
+}
